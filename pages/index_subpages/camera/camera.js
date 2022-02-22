d588b9b755aa23f0aa3d0ed1ed4c58ee59744a35
JackHCC <2508074836@qq.com>
2022-02-22 20:26:52 +0800

    add camera

--- pages/index_subpages/camera/camera.js
+++ pages/index_subpages/camera/camera.js
@@ -5,14 +5,33 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        src: '',//拍照后图像路径(临时路径)
+        show: false//相机视图显示隐藏标识
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        const ctx = wx.createCameraContext()
+        const listener = ctx.onCameraFrame((frame) => {
+          // 每一帧
+          console.log(wx.arrayBufferToBase64(frame.data))
+          // // console.log(frame)
+          console.log(frame.data instanceof ArrayBuffer, frame.width, frame.height)
+          wx.request({
+            url: 'https://xdx.test.com:8000/api/test',
+            method:'POST',
+            data:{
+              base64:wx.arrayBufferToBase64(frame.data)
+            },
+            success:(res)=>{
+              console.log(res)
+            }
+          })
+        //   this.sleep(5000)
+        })
+        listener.start()
     },
 
     /**
